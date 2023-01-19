3f14577ce7f281fe1283c05ddb0e38757708e810
Suraj-Sharma-2404p <86391485+Suraj-Sharma-2404p@users.noreply.github.com>
2023-01-20 02:11:34 +0530

    CustomDirective

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'customDirective';
+
+
+
+number: number = 50;
+isclicked = false;
+
+onclick() {
+  this.isclicked = true;
+  }
 }
